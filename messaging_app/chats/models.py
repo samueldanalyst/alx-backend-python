f2f86fc890b0e6a46179aa50c8857b93b70f5873
samueldanalyst <samuelvictor747futa@gmail.com>
2025-05-29 13:55:51 +0100

    final model update

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -47,10 +47,11 @@ class Conversation(models.Model):
         return f"Conversation {self.id}"
     
 class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sent_messages')
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
 
     def __str__(self):
