9a4b408b0e728262df4c8a9a32dec8431217f795
samueldanalyst <samuelvictor747futa@gmail.com>
2025-06-06 17:54:45 +0100

    filters

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -135,6 +135,9 @@ REST_FRAMEWORK = {
         'chats.auth.CustomJWTAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 
