570b63c970960a1f52fa9782c1337656d28170f7
samueldanalyst <samuelvictor747futa@gmail.com>
2025-06-09 08:06:10 +0100

    malware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -98,7 +98,7 @@ class OffensiveLanguageMiddleware:
 
 
 
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
