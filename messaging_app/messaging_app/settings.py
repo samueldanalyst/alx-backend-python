c21ae6ca5157c6e2e1dbf46010beb918204c5fd6
samueldanalyst <samuelvictor747futa@gmail.com>
2025-06-26 20:40:12 +0100

    Set up Docker Compose for messaging app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -76,12 +76,19 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DB'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'db',  # this is the Docker service name!
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
